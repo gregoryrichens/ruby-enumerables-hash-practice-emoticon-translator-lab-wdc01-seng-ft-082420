b0d037f7f57854403e779fd36f98730d2cc37733
Gregory Richens <gregoryrichens@gmail.com>
2020-04-05 04:22:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
-# require modules here
+require "pry"
+require "yaml"
+emote = YAML.load_file('./lib/emoticons.yml')
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  emote = YAML.load_file(file_path)
+  working_hash = {}
+   emote.each do |(key, emote_array)|
+    emote_array.each do |(english_emote, japanese_emote)|
+      working_hash[key] = {}
+      working_hash[key][:english] = english_emote
+      working_hash[key][:japanese] = japanese_emote
+    end
+  end 
+  working_hash
 end
 
 def get_japanese_emoticon
